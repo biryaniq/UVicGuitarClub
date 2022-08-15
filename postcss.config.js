202cd24d4f60c6f658fcc59c8cc31a40e923e48a
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-08-15 02:02:59 -0700

    set up next.js with tailwind as per https://www.kyrelldixon.com/blog/setup-nextjs-with-tailwindcss-and-typescript

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss', //removes excess CSS
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    'postcss-preset-env',
+  ],
+}
