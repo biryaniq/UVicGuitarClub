ecd0f65b10b51581cb97717f79a20c3d5eec8963
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-08-15 23:01:55 -0700

    added logos, component for guitar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import React from "react";
+import { AppProps } from "next/app";
+import "../styles/index.css";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
